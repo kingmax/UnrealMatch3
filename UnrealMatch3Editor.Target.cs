a8ee52c392f8d879efa57aadb5d0ac2fe607aae8
Jason Li <jason.li@tribe.neobards.cn>
2021-07-01 15:25:03 +0800

    init UnrealMatch3 Source (using 4.26, have some changes, eg: add log)

---
+++ UnrealMatch3Editor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UnrealMatch3EditorTarget : TargetRules
+{
+	public UnrealMatch3EditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		ExtraModuleNames.AddRange( new string[] { "Match3" } );
+
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+    }
+}
